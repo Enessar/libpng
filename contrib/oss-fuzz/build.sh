4347cd59d586bdb0a893bf8f7f4b2cfb17951673
Enessar <rassene.msadaa@epfl.ch>
2025-05-13 13:21:21 +0200

    ....

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -32,7 +32,7 @@ mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
 # build the libpng library.
 autoreconf -f -i
-./configure 
+./configure --with-libpng-prefix=OSS_FUZZ_
 make -j$(nproc) clean
 make -j$(nproc) libpng16.la
 
