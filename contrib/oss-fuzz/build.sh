917cc601c1fa104c8ebd228299078cbb0ef02438
Enessar <rassene.msadaa@epfl.ch>
2025-05-15 02:48:25 +0200

    added seeds for base

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -58,7 +58,7 @@ find $SRC/libpng -name "*.png" | grep -v crashers | \
      xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
 
 find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_BASE_corpus.zip
+     xargs zip $OUT/libpng_read_fuzzer_BASE_seed_corpus.zip
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
